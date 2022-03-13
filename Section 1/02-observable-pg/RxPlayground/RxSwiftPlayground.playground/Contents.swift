756838595a321f102f04474b731e4dbf75a7a126
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-03-13 21:20:47 +0300

    feat: Chapter 2: Observables
    (repeated)

--- Section 1/02-observable-pg/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ Section 1/02-observable-pg/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -6,27 +6,30 @@ example(of: "just, of, from") {
     let two = 2
     let three = 3
     
-    let observable = Observable<Int>.just(one)
-    let observable2 = Observable.of(one, two, three)
-    let observable3 = Observable.of([one, two, three])
-    let observable4 = Observable.from([one, two, three])
+    let _ = Observable<Int>.just(one)
+    let _ = Observable.of(one, two, three)
+    let _ = Observable.of([one, two, three])
+    let _ = Observable.from([one, two, three])
 }
 
 example(of: "subscribe") {
+    
     let one = 1
     let two = 2
     let three = 3
+
     let observable = Observable.of(one, two, three)
-    
+
     observable.subscribe(onNext: { element in
         print(element)
     })
-    
+
 }
 
 example(of: "empty") {
-    let observable = Observable<Void>.empty()
     
+    let observable = Observable<Void>.empty()
+
     observable.subscribe(
         onNext: { element in
             print(element)
@@ -38,20 +41,21 @@ example(of: "empty") {
 }
 
 example(of: "never") {
-    let observable = Observable<Void>.never()
     
+    let observable = Observable<Void>.never()
+
     observable.subscribe(
         onNext: { element in
             print(element)
         },
         onCompleted: {
             print("Completed")
-        }
-    )
+        })
+    
 }
 
 example(of: "range") {
-    
+
     let observable = Observable<Int>.range(start: 1, count: 10)
     observable
         .subscribe(onNext: { i in
@@ -61,6 +65,7 @@ example(of: "range") {
             )
             print(fibonacci)
         })
+    
 }
 
 example(of: "dispose") {
@@ -68,33 +73,34 @@ example(of: "dispose") {
     let subscription = observable.subscribe { event in
         print(event)
     }
-    
+
     subscription.dispose()
 }
 
 example(of: "DisposeBag") {
-    
+
     let disposeBag = DisposeBag()
-    
+
     Observable.of("A", "B", "C")
         .subscribe {
             print($0)
         }
         .disposed(by: disposeBag)
+    
 }
 
 example(of: "create") {
-    
+
     enum MyError: Error {
         case anError
     }
-    
+
     let disposeBag = DisposeBag()
-    
+
     Observable<String>.create { observer in
         observer.onNext("1")
         observer.onNext("?")
-        
+
         return Disposables.create()
     }
     .subscribe(
@@ -104,14 +110,15 @@ example(of: "create") {
         onDisposed: { print("Disposed") }
     )
     .disposed(by: disposeBag)
+    
 }
 
 example(of: "deferred") {
-    
+
     let disposeBag = DisposeBag()
-    
+
     var flip = false
-    
+
     let factory: Observable<Int> = Observable.deferred {
         flip.toggle()
         if flip {
@@ -120,7 +127,7 @@ example(of: "deferred") {
             return Observable.of(4, 5, 6)
         }
     }
-    
+
     for _ in 0...3 {
         factory.subscribe(onNext: {
             print($0, terminator: "")
@@ -128,42 +135,50 @@ example(of: "deferred") {
         .disposed(by: disposeBag)
         print()
     }
+    
 }
 
 example(of: "Single") {
-    
+
     let disposeBag = DisposeBag()
-    
+
     enum FileReadError: Error {
         case fileNotFound, unreadable, encodingFailed
     }
-    
+
     func loadText(from name: String) -> Single<String> {
-        
+
         return Single.create { single in
-            
+
             let disposable = Disposables.create()
-            
-            guard let path = Bundle.main.path(forResource: name, ofType: "txt") else {
+
+            guard
+                let path = Bundle.main.path(forResource: name, ofType: "txt")
+            else {
                 single(.error(FileReadError.fileNotFound))
                 return disposable
             }
-            
-            guard let data = FileManager.default.contents(atPath: path) else {
+
+            guard
+                let data = FileManager.default.contents(atPath: path)
+            else {
                 single(.error(FileReadError.unreadable))
                 return disposable
             }
-            
-            guard let contents = String(data: data, encoding: .utf8) else {
+
+            guard
+                let contents = String(data: data, encoding: .utf8)
+            else {
                 single(.error(FileReadError.encodingFailed))
                 return disposable
             }
-            
+
             single(.success(contents))
             return disposable
         }
+        
     }
-    
+
     loadText(from: "Copyright")
         .subscribe {
             switch $0 {
@@ -174,4 +189,7 @@ example(of: "Single") {
             }
         }
         .disposed(by: disposeBag)
+    
 }
+
+
