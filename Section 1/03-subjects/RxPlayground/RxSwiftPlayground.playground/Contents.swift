481d7196636f9247724655129b31d229092b9d19
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-03-16 20:40:20 +0300

    init: start challenge

--- Section 1/03-subjects/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ Section 1/03-subjects/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -154,3 +154,83 @@ example(of: "BehaviorRelay") {
     print(relay.value) // 2
     
 }
+
+// MARK: - Challenge
+
+example(of: "PublishSubject") {
+    
+    let disposeBag = DisposeBag()
+    
+    let dealtHand = PublishSubject<[(String, Int)]>()
+    
+    func deal(_ cardCount: UInt) {
+        var deck = cards
+        var cardsRemaining = deck.count
+        var hand = [(String, Int)]()
+        
+        for _ in 0..<cardCount {
+            let randomIndex = Int.random(in: 0..<cardsRemaining)
+            hand.append(deck[randomIndex])
+            deck.remove(at: randomIndex)
+            cardsRemaining -= 1
+        }
+        
+    }
+    
+    deal(3)
+}
+
+
+//
+//example(of: "BehaviorRelay") {
+//    
+//    enum UserSession {
+//        case loggedIn, loggedOut
+//    }
+//    
+//    enum LoginError: Error {
+//        case invalidCredentials
+//    }
+//    
+//    let disposeBag = DisposeBag()
+//    
+//    func logInWith(username: String, password: String, completion: (Error?) -> Void) {
+//        guard
+//            username == "johnny@appleseed.com",
+//            password == "appleseed"
+//        else {
+//            completion(LoginError.invalidCredentials)
+//            return
+//        }
+//        
+//        
+//    }
+//    
+//    func logOut() {
+//        
+//    }
+//    
+//    func performActionRequiringLoggedInUser(_ action: () -> Void) {
+//        
+//    }
+//    
+//    for i in 1...2 {
+//        let password = i % 2 == 0 ? "appleseed" : "password"
+//        
+//        logInWith(username: "johnny@appleseed.com", password: password) { error in
+//            guard
+//                error == nil
+//            else {
+//                print(error!)
+//                return
+//            }
+//            
+//            print("User logged in.")
+//        }
+//        
+//        performActionRequiringLoggedInUser {
+//            print("Successfully did something only a logged in user can do.")
+//        }
+//    }
+//    
+//}
