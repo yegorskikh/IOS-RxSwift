81a40f68f9acbef3c10efb4851a4a4a2a3d17a9f
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-03-16 21:24:23 +0300

    feat: challenge subject

--- Section 1/03-subjects/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ Section 1/03-subjects/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -2,6 +2,7 @@ import Foundation
 import RxSwift
 import RxRelay
 
+
 example(of: "PublishSubject") {
     let subject = PublishSubject<String>()
     subject.on(.next("Is anyone listening?"))
@@ -175,62 +176,92 @@ example(of: "PublishSubject") {
             cardsRemaining -= 1
         }
         
+        // Add code to update dealtHand here
+        let handPoints = points(for: hand)
+        if handPoints > 21 {
+            dealtHand.onError(HandError.busted(points: handPoints))
+        } else {
+            dealtHand.onNext(hand)
+        }
     }
+    
+    // Add subscription to handSubject here
+    dealtHand
+        .subscribe(
+            onNext: {
+                print(cardString(for: $0), "for", points(for: $0), "points")
+            },
+            onError: {
+                print(String(describing: $0).capitalized)
+            })
+        .disposed(by: disposeBag)
     
     deal(3)
 }
 
 
-//
-//example(of: "BehaviorRelay") {
-//    
-//    enum UserSession {
-//        case loggedIn, loggedOut
-//    }
-//    
-//    enum LoginError: Error {
-//        case invalidCredentials
-//    }
-//    
-//    let disposeBag = DisposeBag()
-//    
-//    func logInWith(username: String, password: String, completion: (Error?) -> Void) {
-//        guard
-//            username == "johnny@appleseed.com",
-//            password == "appleseed"
-//        else {
-//            completion(LoginError.invalidCredentials)
-//            return
-//        }
-//        
-//        
-//    }
-//    
-//    func logOut() {
-//        
-//    }
-//    
-//    func performActionRequiringLoggedInUser(_ action: () -> Void) {
-//        
-//    }
-//    
-//    for i in 1...2 {
-//        let password = i % 2 == 0 ? "appleseed" : "password"
-//        
-//        logInWith(username: "johnny@appleseed.com", password: password) { error in
-//            guard
-//                error == nil
-//            else {
-//                print(error!)
-//                return
-//            }
-//            
-//            print("User logged in.")
-//        }
-//        
-//        performActionRequiringLoggedInUser {
-//            print("Successfully did something only a logged in user can do.")
-//        }
-//    }
-//    
-//}
+
+example(of: "BehaviorRelay") {
+    enum UserSession {
+        case loggedIn, loggedOut
+    }
+    
+    enum LoginError: Error {
+        case invalidCredentials
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    // Create userSession BehaviorRelay of type UserSession with initial value of .loggedOut
+    let userSession = BehaviorRelay(value: UserSession.loggedOut)
+    
+    // Subscribe to receive next events from userSession
+    userSession
+        .subscribe(onNext: {
+            print("userSession changed:", $0)
+        })
+        .disposed(by: disposeBag)
+    
+    func logInWith(username: String, password: String, completion: (Error?) -> Void) {
+        guard username == "johnny@appleseed.com",
+              password == "appleseed" else {
+                  completion(LoginError.invalidCredentials)
+                  return
+              }
+        
+        // Update userSession
+        userSession.accept(.loggedIn)
+    }
+    
+    func logOut() {
+        // Update userSession
+        userSession.accept(.loggedOut)
+    }
+    
+    func performActionRequiringLoggedInUser(_ action: () -> Void) {
+        // Ensure that userSession is loggedIn and then execute action()
+        guard userSession.value == .loggedIn else {
+            print("You can't do that!")
+            return
+        }
+        
+        action()
+    }
+    
+    for i in 1...2 {
+        let password = i % 2 == 0 ? "appleseed" : "password"
+        
+        logInWith(username: "johnny@appleseed.com", password: password) { error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            print("User logged in.")
+        }
+        
+        performActionRequiringLoggedInUser {
+            print("Successfully did something only a logged in user can do.")
+        }
+    }
+}
