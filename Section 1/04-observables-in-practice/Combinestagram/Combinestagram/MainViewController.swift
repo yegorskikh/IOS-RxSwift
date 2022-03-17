ef3e959e7c0812487b60f8602ab0f2d8d72bd492
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-03-17 21:20:39 +0300

    init: start observables-in-practice

---
+++ Section 1/04-observables-in-practice/Combinestagram/Combinestagram/MainViewController.swift
@@ -0,0 +1,38 @@
+import UIKit
+import RxSwift
+import RxRelay
+
+class MainViewController: UIViewController {
+  
+  @IBOutlet weak var imagePreview: UIImageView!
+  @IBOutlet weak var buttonClear: UIButton!
+  @IBOutlet weak var buttonSave: UIButton!
+  @IBOutlet weak var itemAdd: UIBarButtonItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+  @IBAction func actionClear() {
+    
+  }
+  
+  @IBAction func actionSave() {
+    
+  }
+  
+  @IBAction func actionAdd() {
+    
+  }
+  
+  func showMessage(_ title: String, description: String? = nil) {
+    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Close",
+                                  style: .default,
+                                  handler: { [weak self] _ in
+      self?.dismiss(animated: true, completion: nil)
+    }))
+    present(alert, animated: true, completion: nil)
+  }
+}
