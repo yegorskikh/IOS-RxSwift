6bbfb450ad0eaa62b3244ba2e3b474a3c06b2c4b
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-04-03 13:43:46 +0300

    feat: challenge transforming-operators-in-practice

--- Section 2/08-transforming-operators-in-practice/GitFeed/GitFeed/ActivityController.swift
+++ Section 2/08-transforming-operators-in-practice/GitFeed/GitFeed/ActivityController.swift
@@ -3,7 +3,7 @@ import RxSwift
 import RxCocoa
 import Kingfisher
 
-fileprivate func cachedFileURL(_ fileName: String) -> URL {
+func cachedFileURL(_ fileName: String) -> URL {
   return FileManager.default
     .urls(for: .cachesDirectory, in: .allDomainsMask)
     .first!
@@ -11,7 +11,6 @@ fileprivate func cachedFileURL(_ fileName: String) -> URL {
 }
 
 class ActivityController: UITableViewController {
-  
   private let repo = "ReactiveX/RxSwift"
   
   private let events = BehaviorRelay<[Event]>(value: [])
@@ -19,6 +18,7 @@ class ActivityController: UITableViewController {
   
   private let eventsFileURL = cachedFileURL("events.json")
   private let modifiedFileURL = cachedFileURL("modified.txt")
+  
   private let lastModified = BehaviorRelay<String?>(value: nil)
   
   override func viewDidLoad() {
@@ -27,6 +27,7 @@ class ActivityController: UITableViewController {
     
     self.refreshControl = UIRefreshControl()
     let refreshControl = self.refreshControl!
+    
     refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
     refreshControl.tintColor = UIColor.darkGray
     refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
@@ -51,9 +52,25 @@ class ActivityController: UITableViewController {
     }
   }
   
-  private func fetchEvents(repo: String) {
-    
-    let response = Observable.from([repo])
+  func fetchEvents(repo: String) {
+    let response = Observable
+      .from(["https://api.github.com/search/repositories?q=language:swift&per_page=5"])
+      .map { urlString -> URL in
+        return URL(string: urlString)!
+      }
+      .flatMap { url -> Observable<Any> in
+        let request = URLRequest(url: url)
+        return URLSession.shared.rx.json(request: request)
+      }
+      .flatMap { response -> Observable<String> in
+        guard
+          let response = response as? [String: Any],
+          let items = response["items"] as? [[String: Any]] else {
+          return Observable.empty()
+        }
+        
+        return Observable.from(items.map { $0["full_name"] as! String })
+      }
       .map { urlString -> URL in
         return URL(string: "https://api.github.com/repos/\(urlString)/events")!
       }
@@ -61,8 +78,8 @@ class ActivityController: UITableViewController {
         var request = URLRequest(url: url)
         if let modifiedHeader = self?.lastModified.value {
           request.addValue(modifiedHeader,
-            forHTTPHeaderField: "Last-Modified")
-      }
+                           forHTTPHeaderField: "Last-Modified")
+        }
         return request
       }
       .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
@@ -87,24 +104,21 @@ class ActivityController: UITableViewController {
         return 200..<400 ~= response.statusCode
       }
       .flatMap { response, _ -> Observable<String> in
-        guard
-          let value = response.allHeaderFields["Last-Modified"] as? String
-        else {
+        guard let value = response.allHeaderFields["Last-Modified"] as? String else {
           return Observable.empty()
         }
-        
         return Observable.just(value)
       }
       .subscribe(onNext: { [weak self] modifiedHeader in
         guard let self = self else { return }
+        
         self.lastModified.accept(modifiedHeader)
         try? modifiedHeader.write(to: self.modifiedFileURL, atomically: true, encoding: .utf8)
       })
       .disposed(by: bag)
   }
   
-  private func processEvents(_ newEvents: [Event]) {
-    
+  func processEvents(_ newEvents: [Event]) {
     var updatedEvents = newEvents + events.value
     
     if updatedEvents.count > 50 {
@@ -121,11 +135,9 @@ class ActivityController: UITableViewController {
     if let eventsData = try? JSONEncoder().encode(updatedEvents) {
       try? eventsData.write(to: eventsFileURL, options: .atomicWrite)
     }
-    
   }
   
   // MARK: - Table Data Source
-  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return events.value.count
   }
@@ -139,4 +151,5 @@ class ActivityController: UITableViewController {
     cell.imageView?.kf.setImage(with: event.actor.avatar, placeholder: UIImage(named: "blank-avatar"))
     return cell
   }
+  
 }
