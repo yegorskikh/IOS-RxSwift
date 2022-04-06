8a7b6b3115ca5369b2925a921e2bd1b11c784066
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-04-06 21:28:15 +0300

    feat: challenges time-based-operators

--- Section 2/11-time-based-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Pages/buffer.xcplaygroundpage/Contents.swift
+++ Section 2/11-time-based-operators/RxSwiftPlayground/RxSwiftPlayground.playground/Pages/buffer.xcplaygroundpage/Contents.swift
@@ -2,10 +2,38 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-// Start coding here
+let bufferTimeSpan: RxTimeInterval = .seconds(4)
+let bufferMaxCount = 2
 
+let sourceObservable = PublishSubject<String>()
 
-// Support code 
+let sourceTimeline = TimelineView<String>.make()
+let bufferedTimeline = TimelineView<Int>.make()
+
+let stack = UIStackView.makeVertical([
+    UILabel.makeTitle("buffer"),
+    UILabel.make("Emitted elements:"),
+    sourceTimeline,
+    UILabel.make("Buffered elements (at most \(bufferMaxCount) every \(bufferTimeSpan) seconds):"),
+    bufferedTimeline])
+
+_ = sourceObservable.subscribe(sourceTimeline)
+
+sourceObservable
+    .buffer(timeSpan: bufferTimeSpan, count: bufferMaxCount, scheduler: MainScheduler.instance)
+    .map(\.count)
+    .subscribe(bufferedTimeline)
+
+let hostView = setupHostView()
+hostView.addSubview(stack)
+hostView
+
+let elementsPerSecond = 0.7
+let timer = DispatchSource.timer(interval: 1.0 / Double(elementsPerSecond), queue: .main) {
+    sourceObservable.onNext("🐱")
+}
+
+// Support code -- DO NOT REMOVE
 class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConvertible {
     
     static func make() -> TimelineView<E> {
